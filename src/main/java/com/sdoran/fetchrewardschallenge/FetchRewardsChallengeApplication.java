7461c8ec082b5697187787e207b388890cde4eb8
Sean Doran <sdoran35@gmail.com>
2022-04-07 19:31:43 -0500

    Commit basic project structure

---
+++ src/main/java/com/sdoran/fetchrewardschallenge/FetchRewardsChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.sdoran.fetchrewardschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchRewardsChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FetchRewardsChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sdoran/fetchrewardschallenge/FetchRewardsChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdoran.fetchrewardschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchRewardsChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
